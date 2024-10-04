7cc619aebff7b2bd3242d10c60f8ac585583a0b6
Roy Russo <royrusso@gmail.com>
2024-10-04 07:19:18 -0400

    - init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_parquet
+
 a Python, Parquet, and Pandas playground
+
+CSV from https://github.com/datablist/sample-csv-files
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+CSV_FILE = 'people-1000.csv'
+PARQUET_FILE = 'people-1000.parquet'
+
+# read CSV into pandas
+df = pd.read_csv(CSV_FILE)
+
+# convert to pyarrow table
+table = pa.Table.from_pandas(df)
+
+# write to parquet
+pq.write_table(table, PARQUET_FILE)
+
+# read parquet file
+table2 = pq.read_table(PARQUET_FILE)
+
+# convert to pandas
+df2 = table2.to_pandas()
+
+print(df2.head())
+
+# read metadata
+print(table2.schema)
+pfile = pq.ParquetFile(PARQUET_FILE)
+print(pfile.metadata)
+print(pfile.schema)
+
